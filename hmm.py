62f86d1203d2ba7bc7bda05c35b1408e0a774a7d
lsr <1834118348@qq.com>
2019-11-13 00:57:08 +0800

    ghmm

--- hmm.py
+++ hmm.py
@@ -48,13 +48,14 @@ class HMM(object):
             = alpha(t,i) * belta(t,i)
         3.  P(o1,...,oT) 
             = sum_i alpha(t,i) * belta(t,i)
+        
         epsilon, gamma可用alpha, belta表示
         epsilon[k,t,i,j] = P(zt = i, zt+1 = j | Qk) = P(zt = i, zt+1 = j, Qk) / P(Qk) 
                          = alpha(t,i) * a(i,j) * b(t+1,j) * belta(t+1,j) / P(Qk)
                          = alpha(t,i) * a(i,j) * b(t+1,j) * belta(t+1,j) / sum_i alpha(Tk, i)
         gamma[k,t,i] = P(zt = i| Qk) 
                      = sum_j epsilon[k,t,i,j]
-    M部在子类中根据不同的发射概率模型实现
+    M部在子类中根据不同的发射概率模型实现 
     '''
     def expect(self, Qs):
         alphas = self.forward(Qs)
